87e84e27c1c793384742ecf4a6d15f8ed373ef1b
Alpacow <fvpereira@inf.ufsm.br>
2019-11-05 17:31:50 -0200

    Att PAA

--- DFS/dfs.py
+++ DFS/dfs.py
@@ -1,69 +1,38 @@
 from collections import defaultdict 
-   
-#This class represents a directed graph  
-# using adjacency list representation 
+
+
 class Graph: 
-   
-    def __init__(self,vertices): 
-        #No. of vertices 
-        self.V= vertices  
-          
-        # default dictionary to store graph 
-        self.graph = defaultdict(list)  
-   
-    # function to add an edge to graph 
-    def addEdge(self,u,v): 
+    def __init__(self): 
+        self.graph = defaultdict(list) # grafo = dicionário com listas de vertices
+
+    def DFS(self, v): 
+        explored = [False] * (len(self.graph)) # seta todos os vertices como não visitados
+        # chama recursivamente a funçao auxiliar
+        # que marca os nodos visitados
+        self.Explored(v, explored) 
+  
+    def Explored(self, v, explored): 
+        explored[v] = True # seta o nodo atual como Visitado
+        print(v, self.graph[v]) 
+        # Realiza a recursão para percorrer os vertices adjacentes
+        for i in self.graph[v]: 
+            if explored[i] == False: # se não foi explorado chama recursão
+                self.Explored(i, explored)
+
+    def AddEdge(self, u, v): 
         self.graph[u].append(v) 
-   
-    '''A recursive function to print all paths from 'u' to 'd'. 
-    visited[] keeps track of vertices in current path. 
-    path[] stores actual vertices and path_index is current 
-    index in path[]'''
   
+
+# Testes
+g = Graph() 
+g.AddEdge(0, 1) 
+g.AddEdge(0, 2)
+g.AddEdge(0, 3)
+g.AddEdge(1, 2) 
+g.AddEdge(2, 0)
+g.AddEdge(3, 3)
+g.AddEdge(2, 4)
+g.AddEdge(4, 4)
   
-                      
-                    self.printAllPathsUtil(i, d, visited, path) 
-                if visited[i]==False: 
-            # If current vertex is not destination 
-            #Recur for all the vertices adjacent to this vertex 
-            for i in self.graph[u]: 
-            print path 
-        # If current vertex is same as destination, then print 
-        # Mark the current node as visited and store in path 
-        # Remove current vertex from path[] and mark it as unvisited 
-        # current path[] 
-        else: 
-        if u ==d: 
-        path.append(u) 
-        path.pop() 
-        visited[u]= False
-        visited[u]= True
-    def printAllPathsUtil(self, u, d, visited, path): 
-   
-   
-    # Prints all paths from 's' to 'd' 
-    def printAllPaths(self,s, d): 
-  
-        # Mark all the vertices as not visited 
-        visited =[False]*(self.V) 
-  
-        # Create an array to store paths 
-        path = [] 
-  
-        # Call the recursive helper function to print all paths 
-        self.printAllPathsUtil(s, d,visited, path) 
-   
-   
-   
-# Create a graph given in the above diagram 
-g = Graph(4) 
-g.addEdge(0, 1) 
-g.addEdge(0, 2) 
-g.addEdge(0, 3) 
-g.addEdge(2, 0) 
-g.addEdge(2, 1) 
-g.addEdge(1, 3) 
-   
-s = 2 ; d = 3
-print ("Following are all different paths from %d to %d :" %(s, d)) 
-g.printAllPaths(s, d) 
+print("DFS - Início: 0") 
+g.DFS(0) 
