0f772b0bd3408c6265ef305dfb5161faf5a35605
Francielle Vasconcellos Pereira <fvpereira@lenovo01.inf.ufsm.br>
2019-11-11 11:13:21 -0200

    Retirados pastas inuteis

---
+++ strongConnect.py
@@ -0,0 +1,36 @@
+from collections import defaultdict 
+
+
+class Graph: 
+    def __init__(self): 
+        self.graph = defaultdict(list) # grafo = dicionário com listas de vertices
+
+    def DFS(self, v): 
+        explored = [False] * (len(self.graph)) # seta todos os vertices como não visitados
+        # chama recursivamente a funçao auxiliar
+        # que marca os nodos visitados
+        self.Explored(v, explored) 
+  
+    def Explored(self, v, explored): 
+        explored[v] = True # seta o nodo atual como Visitado
+        print(v, self.graph[v]) 
+        # Realiza a recursão para percorrer os vertices adjacentes
+        for i in self.graph[v]: 
+            if explored[i] == False: # se não foi explorado chama recursão
+                self.Explored(i, explored)
+
+    def AddEdge(self, u, v): 
+        self.graph[u].append(v) 
+  
+
+# Testes
+g = Graph() 
+g.AddEdge(0, 1); 
+g.AddEdge(1, 2); 
+g.AddEdge(2, 3); 
+g.AddEdge(3, 0); 
+g.AddEdge(2, 4); 
+g.AddEdge(4, 2); 
+  
+print("DFS - Início: 0") 
+g.DFS(0) 
