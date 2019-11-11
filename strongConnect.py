a772d298163948c06b72c57ef16bce70b81e57ef
Alpacow <fvpereira@inf.ufsm.br>
2019-11-11 11:14:20 -0200

    Add trab de grafo fortemente conectado

--- strongConnect.py
+++ strongConnect.py
@@ -2,35 +2,55 @@ from collections import defaultdict
 
 
 class Graph: 
-    def __init__(self): 
+    def __init__(self, nrV): 
         self.graph = defaultdict(list) # grafo = dicionário com listas de vertices
+        self.V = nrV
 
-    def DFS(self, v): 
-        explored = [False] * (len(self.graph)) # seta todos os vertices como não visitados
+    def DFS(self, v, graph): 
+        explored = [False] * (self.V) # seta todos os vertices como não visitados
         # chama recursivamente a funçao auxiliar
         # que marca os nodos visitados
-        self.Explored(v, explored) 
+        self.Explored(v, explored, graph)
+        return False if False in explored else True
   
-    def Explored(self, v, explored): 
+    def Explored(self, v, explored, graph): 
         explored[v] = True # seta o nodo atual como Visitado
-        print(v, self.graph[v]) 
+        print(v, graph[v])
+        print(explored)
         # Realiza a recursão para percorrer os vertices adjacentes
-        for i in self.graph[v]: 
+        for i in graph[v]: 
             if explored[i] == False: # se não foi explorado chama recursão
-                self.Explored(i, explored)
+                self.Explored(i, explored, graph)
+
+    def ReverseGraph(self):
+        newG = Graph(self.V)
+        for i in self.graph: 
+            for j in self.graph[i]: 
+                newG.AddEdge(j,i)
+        return newG
 
     def AddEdge(self, u, v): 
-        self.graph[u].append(v) 
-  
+        self.graph[u].append(v)
+
+    def isStrongConnect(self, init):
+        if self.DFS(init, self.graph):
+            newG = self.ReverseGraph()
+            return self.DFS(init, newG.graph)
+        return False
+
 
 # Testes
-g = Graph() 
+g = Graph(5) 
 g.AddEdge(0, 1); 
 g.AddEdge(1, 2); 
 g.AddEdge(2, 3); 
 g.AddEdge(3, 0); 
 g.AddEdge(2, 4); 
-g.AddEdge(4, 2); 
+g.AddEdge(4, 2);
+print("Fortemente conectado\n" if g.isStrongConnect(0) else "Não fortemente conectado\n")
   
-print("DFS - Início: 0") 
-g.DFS(0) 
+g2 = Graph(4) 
+g2.AddEdge(0, 1) 
+g2.AddEdge(1, 2) 
+g2.AddEdge(2, 3)
+print ("Fortemente conectado\n" if g2.isStrongConnect(0) else "Não fortemente conectado\n")
